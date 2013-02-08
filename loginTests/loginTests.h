6055117ba70933d2152c597ae2610bc8c371b2ab
vivekdharman <vivekdharman@vivekdharmans-MacBook-Pro.local>
2013-02-08 20:33:05 +0530

    Initial Commit

---
+++ loginTests/loginTests.h
@@ -0,0 +1,13 @@
+//
+//  loginTests.h
+//  loginTests
+//
+//  Created by vivekdharman on 08/02/13.
+//  Copyright (c) 2013 vivekdharman. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface loginTests : SenTestCase
+
+@end
---
+++ loginTests/loginTests.m
@@ -0,0 +1,32 @@
+//
+//  loginTests.m
+//  loginTests
+//
+//  Created by vivekdharman on 08/02/13.
+//  Copyright (c) 2013 vivekdharman. All rights reserved.
+//
+
+#import "loginTests.h"
+
+@implementation loginTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in loginTests");
+}
+
+@end
